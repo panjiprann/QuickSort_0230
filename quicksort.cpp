a30eee3d680178a21094b70db998d4b0b9fe4a20
panjiprann <panjipranantya20@gmail.com>
2024-03-28 16:12:13 +0700

    masukan semua fungsi dan prosedur ke main

--- quicksort.cpp
+++ quicksort.cpp
@@ -97,3 +97,25 @@ void display()
     cout << "\n\nnumber of comparasions: " << cmp_count << endl;
     cout << "number of data movements: " << mov_count << endl;
 }
+int main()
+{
+    char ch;
+
+    do{
+        input();
+        q_short(0, n - 1);
+        display();
+        cout  << "\n\nDo you want to continue? (y/n)";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+        break;
+
+        system("pause");
+        system("cls");
+
+    }while (true);
+
+    return 0;
+}
+    
+
