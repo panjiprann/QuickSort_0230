4526862d67b3f9baf78c37fe1cd203bb2fa939c1
panjiprann <panjipranantya20@gmail.com>
2024-03-28 15:10:58 +0700

    function main

--- quicksort.cpp
+++ quicksort.cpp
@@ -5,3 +5,18 @@ using namespace std;
 int arr[20]; 
 int cmp_count = 0; // number of comparision
 int mov_count = 0; //number of moves
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "masukan panjang element array: ";
+        cin >> n;
+
+        if (n<=20)
+        break;
+        else
+        cout << "\nmaksimum panjang array adalah 20" << endl;
+    }    
+    
+}
