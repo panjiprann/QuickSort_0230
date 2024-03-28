f91d7ff8e0c8ab7f79f2a09aaf5e50107aa1e26c
panjiprann <panjipranantya20@gmail.com>
2024-03-28 15:58:14 +0700

    membuat step 13

--- quicksort.cpp
+++ quicksort.cpp
@@ -80,4 +80,5 @@ void q_short( int low, int high )
     //recursive call to sort the lefts sub array
     q_short(low, j - 1);  //step 12
     //recursive call to sort the rights sub array
+    q_short(j + 1, high); //step 13
 }
